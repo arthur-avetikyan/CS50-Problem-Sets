6465ef2183a9b6b7973286f0d79326d9eada8166
arthur-avetikyan <arthur-avetikyan@users.noreply.github.com>
2020-03-06 12:06:29 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -10,6 +10,7 @@ const BYTE second_byte = 0xd8;
 const BYTE third_byte = 0xff;
 const int block = 512;
 
+void access_memory(FILE *file, BYTE *memory_block, int *file_name, bool *counter, char *name, int *size);
 void recover_image(BYTE *memory_block, int *file_name, bool *counter, char *name);
 
 int main(int argc, char *argv[])
@@ -27,43 +28,47 @@ int main(int argc, char *argv[])
     }
 
     BYTE *memory_block = malloc(block);
-    char *name = malloc(8*sizeof(char));
+    char *name = malloc(8 * sizeof(char));
     int file_name = 0;
     bool counter = false;
     fseek(file, 0, SEEK_END);
     int size = ftell(file);
-
     rewind(file);
 
-    int it_count= 0;
-    for (int i = 0; i < size / block; i++)
-    {
-        fread(memory_block, sizeof(char), block, file);
-
-        if (memory_block[0] == first_byte && memory_block[1] == second_byte && memory_block[2] == third_byte && !counter)
-        {
-            recover_image(memory_block, &file_name, &counter, name);
-        }
-        else if (counter)
-        {
-            recover_image(memory_block, &file_name, &counter, name);
-        }
-        it_count ++;
-    }
-
-    printf("%i\n", it_count);
+    access_memory(file, memory_block, &file_name, &counter, name, &size);
 
     free(memory_block);
     free(name);
     fclose(file);
 }
 
+void access_memory(FILE *file, BYTE *memory_block, int *file_name, bool *counter, char *name, int *size)
+{
+    int it_count = 0;
+    for (int i = 0; i < *size / block; i++)
+    {
+        fread(memory_block, sizeof(char), block, file);
+
+        if (memory_block[0] == first_byte && memory_block[1] == second_byte && memory_block[2] == third_byte && !*counter)
+        {
+            recover_image(memory_block, file_name, counter, name);
+        }
+        else if (*counter)
+        {
+            recover_image(memory_block, file_name, counter, name);
+        }
+        it_count ++;
+    }
+
+    printf("%i\n", it_count);
+}
 
 void recover_image(BYTE *memory_block, int *file_name, bool *counter, char *name)
 {
     if (!*counter)
     {
-        *file_name < 10 ? sprintf(name, "00%i.jpg", *file_name): *file_name > 10 && *file_name < 100 ? sprintf(name, "0%i.jpg", *file_name): sprintf(name, "%i.jpg", *file_name);
+        *file_name < 10 ? sprintf(name, "00%i.jpg", *file_name) : *file_name > 10
+        && *file_name < 100 ? sprintf(name, "0%i.jpg", *file_name) : sprintf(name, "%i.jpg", *file_name);
     }
 
     FILE *image = fopen(name, "a");
@@ -75,6 +80,7 @@ void recover_image(BYTE *memory_block, int *file_name, bool *counter, char *name
             *file_name += 1;
             *counter = false;
             fclose(image);
+            recover_image(memory_block, file_name, counter, name);
             return;
         }
         fputc(memory_block[y], image);
