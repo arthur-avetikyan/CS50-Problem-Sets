f93b4a89aaca96556814487b07a0ad431f5136b6
arthur-avetikyan <arthur-avetikyan@users.noreply.github.com>
2020-03-12 12:27:28 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -4,7 +4,7 @@ def main():
     letters = count_letters(text)
     words = count_words(text)
     sentences = count_sentences(text)
-    index = int(0.0588 * int(letters * 100 / words) - 0.296 * int(sentences * 100 / words) - 15.8)
+    index = int(0.0588 * (letters * 100 / words) - 0.296 * (sentences * 100 / words) - 15.8)
 
     if index < 1:
         print("Before Grade 1")
