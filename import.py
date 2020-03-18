e5f65edf862d73dc12e45c9ccfdc6fa15ed462d4
arthur-avetikyan <arthur-avetikyan@users.noreply.github.com>
2020-03-18 18:07:49 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -15,14 +15,15 @@ with open(argv[1], "r") as file:
         val = row['name']
         if val.count(' ') == 2:
             first_name = val[0: val.index(" ")]
-            name = val[len(first_name) + 1 : len(val)]
-            middle_name = name[0 : name.index(" ")]
-            last_name = name[len(middle_name)+1:len(name)]
+            name = val[len(first_name) + 1: len(val)]
+            middle_name = name[0: name.index(" ")]
+            last_name = name[len(middle_name) + 1: len(name)]
         else:
-            first_name = val[0 : val.index(" ")]
-            name = val[len(first_name) + 1 : len(val)]
+            first_name = val[0: val.index(" ")]
+            name = val[len(first_name) + 1: len(val)]
             middle_name = None
-            last_name = name[0:len(name)]
-        db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)", first_name, middle_name, last_name, row['house'], row['birth'])
+            last_name = name[0: len(name)]
+        db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)",
+                   first_name, middle_name, last_name, row['house'], row['birth'])
 
 exit(0)
