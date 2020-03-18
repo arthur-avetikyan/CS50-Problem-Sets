763ad5a40c1fba62b50e90b3de301acfd60411a7
arthur-avetikyan <arthur-avetikyan@users.noreply.github.com>
2020-03-18 15:48:25 +0000

    automated commit by submit50

--- 6.sql
+++ 6.sql
@@ -1,2 +1,5 @@
 SELECT AVG(rating) FROM ratings
-WHERE movie_id IN (SELECT id FROM movies WHERE year >= 2012)
+WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012)
+
+-- JOIN movies ON ratings.movie_id = movies.id
+-- WHERE movies.year = 2012
--- 7.sql
+++ 7.sql
@@ -1,5 +1,5 @@
 SELECT title, ratings.rating
-FROM (SELECT * FROM movies WHERE movies.year = 2010)
-JOIN ratings
-ON ratings.movie_id = id AND ratings.rating NOT NULL
-ORDER BY ratings.rating, title DESC
+FROM movies
+JOIN ratings ON ratings.movie_id = id AND ratings.rating NOT NULL
+WHERE movies.year = 2010
+ORDER BY ratings.rating DESC, title
