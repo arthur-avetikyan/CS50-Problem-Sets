2a3494445754881b2971480396f98763d03b58a8
arthur-avetikyan <arthur-avetikyan@users.noreply.github.com>
2020-05-04 19:21:26 +0400

    automated commit by submit50

---
+++ Assets/LevelIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelIdentifier : MonoBehaviour
+{
+    public static int currentLevel = 1;
+    public Text instruction;
+
+    void Awake()
+    {
+        instruction.text = string.Format("You are on maze {0}", currentLevel);
+    }
+}
---
+++ Assets/LoadSceneOnInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnInput : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Submit") == 1)
+        {
+            var sceneName = SceneManager.GetActiveScene().name;
+            if (sceneName == "Title")
+            {
+                SceneManager.LoadScene("Play");
+            }
+            else
+            {
+                SceneManager.LoadScene("Title");
+            }
+
+        }
+    }
+}
